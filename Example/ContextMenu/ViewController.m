146899d0efcb1cef2b8dbaab45460b23ba3c8931
Ivan Nesterenko <therealnesterenko@gmail.com>
2015-03-16 19:14:26 +0200

    iPad appearance fixed #5

--- Example/ContextMenu/ViewController.m
+++ Example/ContextMenu/ViewController.m
@@ -132,6 +132,7 @@ static NSString *const menuCellIdentifier = @"rotationCell";
     ContextMenuCell *cell = [tableView dequeueReusableCellWithIdentifier:menuCellIdentifier forIndexPath:indexPath];
     
     if (cell) {
+        cell.backgroundColor = [UIColor clearColor];
         cell.menuTitleLabel.text = [self.menuTitles objectAtIndex:indexPath.row];
         cell.menuImageView.image = [self.menuIcons objectAtIndex:indexPath.row];
     }
